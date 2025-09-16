0b89bf06b756c8b1081dbfdd6297e499dd4f529b
OTAVIO DOS SANTOS LUIZ <otavio.luiz@alunos.sc.senac.br>
2025-09-16 16:44:53 -0300

    perfil quase pronto, terminar amanhã

--- src/app/perfil/perfil.page.ts
+++ src/app/perfil/perfil.page.ts
@@ -1,14 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonCard, IonIcon, IonList, IonItem, IonToolbar,IonImg, IonLabel } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonCard, IonIcon,IonTab, IonTabBar, IonTabs, IonTabButton, IonList, IonItem, IonToolbar,IonImg, IonLabel } from '@ionic/angular/standalone';
+import { defineCustomElements } from '@ionic/core/loader';
+
+import { addIcons } from 'ionicons';
+import { logoIonic } from 'ionicons/icons';
+
+import '@ionic/core/css/core.css';
+
+import '@ionic/core/css/normalize.css';
+import '@ionic/core/css/structure.css';
+import '@ionic/core/css/typography.css';
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.page.html',
   styleUrls: ['./perfil.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar,IonList, IonIcon, IonCard, IonItem, IonLabel, IonImg, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar,IonList, IonTab, IonTabBar, IonTabs, IonTabButton, IonIcon, IonCard, IonItem, IonLabel, IonImg, CommonModule, FormsModule]
 })
 export class PerfilPage implements OnInit {
 
