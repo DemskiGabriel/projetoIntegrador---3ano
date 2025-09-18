3b7070c76c6ac2d22210cd6720fd1b77e53f1015
OTAVIO DOS SANTOS LUIZ <otavio.luiz@alunos.sc.senac.br>
2025-09-18 09:47:38 -0300

    adicionei o routerlink

--- src/app/perfil/perfil.page.ts
+++ src/app/perfil/perfil.page.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonCard, IonIcon,IonTab, IonTabBar,IonFab, IonFabButton, IonTabs, IonTabButton, IonList, IonItem, IonToolbar,IonImg, IonLabel } from '@ionic/angular/standalone';
 import { defineCustomElements } from '@ionic/core/loader';
+import { Router, RouterLink } from '@angular/router';
 
 import { addIcons } from 'ionicons';
 import { logoIonic } from 'ionicons/icons';
@@ -18,7 +19,7 @@ import '@ionic/core/css/typography.css';
   templateUrl: './perfil.page.html',
   styleUrls: ['./perfil.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar,IonList, IonTab, IonTabBar, IonTabs, IonFab, IonFabButton, IonTabButton, IonIcon, IonCard, IonItem, IonLabel, IonImg, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar,IonList,RouterLink, IonTab, IonTabBar, IonTabs, IonFab, IonFabButton, IonTabButton, IonIcon, IonCard, IonItem, IonLabel, IonImg, CommonModule, FormsModule]
 })
 export class PerfilPage implements OnInit {
 
